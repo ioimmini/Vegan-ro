4e1466be4fcb10d1aee99c23739803ba7c480255
Jeongmin CHOI <141547885+ioimmini@users.noreply.github.com>
2024-05-31 16:10:48 +0900

    Elice_2차_Project

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'eslint-plugin-prettier',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', 'eslint-plugin-prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/prop-types': 'off',
+  },
+};
